2ba6acb33755ed99e3f38a3650162aab3bf98923
Ian Hancock <northroadian@gmail.com>
2021-09-23 06:40:44 -0500

    axios works

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 function App() {
   // the state to save the Api data 
   const [apiData, setApiData] = useState(null)
 
   // the useEffect runs when the component mounts and sets the state when its requested.
-  useEffect(()=>{
-    //fetch the data
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(res => res.json())
-    .then(data => setApiData(data))
+//   useEffect(()=>{
+//     fetch the data
+//     fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then(res => res.json())
+//     .then(data => setApiData(data))
+// },[])
+
+//*** AXIOS */
+useEffect(() => {
+  axios("https://jsonplaceholder.typicode.com/posts")
+  .then(data=> setApiData(data.data))
 },[])
+
 console.log(apiData)
   return (
     <div>
@@ -21,3 +29,22 @@ console.log(apiData)
 }
 
 export default App;
+
+
+
+// async function awesome (){
+//   try{
+//     const data = await Promise
+//     return [data, null];
+//   } catch(error){
+//     return [null ,error]
+//   }
+// }
+
+// async function main(){
+//   const [data,error] = await awesome
+//   if(error) {
+//     const [data2,error2] = await awesome()
+//     const [data3,error3] = await awesome()
+//   }
+// }
